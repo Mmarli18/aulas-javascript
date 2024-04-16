eb1699fc9bae80204984786ddcc4a7102b3f14db
Mmarli18 <mahmorais0@gmail.com>
2024-04-16 17:14:15 -0300

    Exercicios

---
+++ js/arrays.js
@@ -0,0 +1,24 @@
+'use script';
+
+// Sinal de colchete [] significa uma lista
+const alunos = ["Wilian", "Vínicius", "Marli", "Duda", "Gabriel"];
+
+// Exibidos a estrutura do array (somente no console)
+console.log(alunos);
+
+// Acessando individualmente alguns alunos
+console.log(alunos[1]);
+console.log(alunos[3]);
+console.log(alunos[0]);
+
+// Exercicio
+
+const hobby = ["Leitura", "Andar de Bike", "Filmes", "Viajar", "Barzinho", "Shopping", "Sertaneja"];
+
+console.log(hobby);
+
+console.log(`Meu hobby favorito é ${hobby[1]} curtir um ${hobby[4]} com muita música ${hobby[6]}.`)
+
+
+
+
